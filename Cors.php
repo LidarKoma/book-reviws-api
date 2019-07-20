85f764e6e1035b3647fe69eae3a68a6cf06cd0f9
LidarKoma <lidarkoma@yahoo.com>
2019-07-20 16:40:49 -0500

    Add files via upload

--- Cors.php
+++ Cors.php
@@ -7,7 +7,8 @@ use Closure;
 class Cors {
    public function handle($request, Closure $next)
    {
-       header("Access-Control-Allow-Origin: *");
+    
+       header("Access-Control-Allow-Origin: https://book-reviws-api.herokuapp.com/api/categories/");
        header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
        header('Access-Control-Allow-Credentials: true');
